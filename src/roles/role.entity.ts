3621d5c75543fd6124d416b39311760a2b4157dd
asma <asmazaied@outlook.fr>
2024-12-17 10:45:03 +0100

    ajout des roles

---
+++ src/roles/role.entity.ts
@@ -0,0 +1,37 @@
+import { Association } from "src/associations/association.entity";
+import { User } from "src/users/user.entity";
+import { Column, Entity, JoinTable, ManyToOne, OneToMany, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Role{
+    @PrimaryColumn()
+    public id_user: number;
+
+    @PrimaryColumn()
+    public id_association: number;
+
+    @Column()
+    public name: string;
+    
+    @ManyToOne(type => User, {eager: true})
+    public user: User;
+
+    @ManyToOne(type => Association, {eager: true})
+    public association: Association;
+
+    /*
+    constructor(
+        name: string,
+        o_user: User,
+        o_association: Association
+    ){
+        //let id: number = o_association.getId()
+        this.name = name,
+        this.user = o_user,
+        this.association = o_association,
+        this.id_user = o_user.id,
+        this.id_association = o_association.id
+    }
+        */
+    
+}
